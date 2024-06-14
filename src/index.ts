02d8090709ef68f49b17e12542893bb33d098ce6
rushilhacker <rushiling121@gmail.com>
2024-06-14 16:08:40 +0530

    null | void

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,9 @@
 import { Hono } from 'hono'
-import { serveStatic } from 'hono/bun'
 
 const app = new Hono()
-const css = new Hono()
-const js = new Hono()
 
 
 app.get('/', (c) => c.text('Hello Bun!'))
-app.get('/a', serveStatic({path :'/client/pages/signup.htm'}))
-
-css.get('/signup.css', serveStatic({path : '/client/style/signup.css'}))
-
-js.get('/signup.js', serveStatic({path: '/client/js/signup.js'}))
-
-app.route('/js', js)
-app.route('/css', css)
 
 const port = process.env.PORT
 
